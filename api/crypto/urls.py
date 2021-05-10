154ff769f5c94af0cdfd6bfec690f4bcc1aa134d
konaitis <konaitis@twilio.com>
2021-05-10 11:17:51 +0300

    * Rename & document endpoints

--- api/crypto/urls.py
+++ api/crypto/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import re_path
 from .views import AddressTransactionsView, TransactionsView, AddressSearchView, TransactionSearchView
 
 urlpatterns = [
-    re_path(r'(?P<crypto>(btc|eth|bch))/address/(?P<address>\w+)/transactions/$', AddressTransactionsView.as_view(),
+    re_path(r'(?P<crypto>(btc|eth|bch))/addresses/(?P<address>\w+)/transactions/$', AddressTransactionsView.as_view(),
             name='address-transactions'),
     re_path(r'searches/addresses/$', AddressSearchView.as_view(), name='address-transactions-log'),
-    re_path(r'(?P<crypto>(btc|eth|bch))/transaction/(?P<tx>\w+)/$', TransactionsView.as_view(),
+    re_path(r'(?P<crypto>(btc|eth|bch))/transactions/(?P<tx>\w+)/$', TransactionsView.as_view(),
             name='transaction-detail'),
     re_path(r'searches/transactions/$', TransactionSearchView.as_view(), name='transaction-log'),
 ]
